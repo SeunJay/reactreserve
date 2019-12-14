f2f91c33d3afa02b3afa7f8264628c51bea92799
seun jay <seunjay92@gmail.com>
2019-12-14 12:21:12 +0100

    try and catch

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -3,19 +3,23 @@ import mongoose from "mongoose";
 const connection = {};
 
 async function connectDB() {
-  if (connection.isConnected) {
-    console.log("Using existing connection");
-    return;
-  }
+  try {
+    if (connection.isConnected) {
+      console.log("Using existing connection");
+      return;
+    }
 
-  const db = await mongoose.connect(process.env.MONGO_SRV, {
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
-  console.log("DB connected!");
-  connection.isConnected = db.connections[0].readyState;
+    const db = await mongoose.connect(process.env.MONGO_SRV, {
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("DB connected!");
+    connection.isConnected = db.connections[0].readyState;
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export default connectDB;

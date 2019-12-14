c2a4bf580a402c137dc5c4bd98a183fe308aa7b4
seun jay <seunjay92@gmail.com>
2019-12-14 16:50:22 +0100

    update url

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ProductList from "../components/Index/ProductList";
 import axios from "axios";
+import baseURL from "../utils/baseUrl"
 
 function Home({ products }) {
   return <ProductList products={products} />;
 }
 
 Home.getInitialProps = async function() {
-  const url = "http://localhost:3000/api/products";
+  const url = `${baseURL}/api/products`;
   const response = await axios.get(url);
   return { products: response.data };
 };

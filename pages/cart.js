98a6fd8bdf3b34433231fb69303cd088ec2a5942
seun jay <seunjay92@gmail.com>
2019-12-31 10:36:15 +0100

    create method for removing products from cart

--- pages/cart.js
+++ pages/cart.js
@@ -1,16 +1,35 @@
+import React, { useState } from "react";
 import { Segment } from "semantic-ui-react";
 import CartItemList from "../components/Cart/CartItemList";
 import CartSummary from "../components/Cart/CartSummary";
 import { parseCookies } from "nookies";
 import baseUrl from "../utils/baseUrl";
 import axios from "axios";
+import cookie from "js-cookie";
 
-function Cart({ products }) {
-  console.log(products);
+function Cart({ products, user }) {
+  const [cartProducts, setCartProducts] = useState(products);
+
+  async function handleRemoveFromCart(productId) {
+    const url = `${baseUrl}/api/cart`;
+    const token = cookie.get("token");
+    const payload = {
+      params: { productId },
+      headers: { Authorization: token }
+    };
+
+    const response = await axios.delete(url, payload);
+    setCartProducts(response.data);
+  }
+
   return (
     <Segment>
-      <CartItemList />
-      <CartSummary />
+      <CartItemList
+        handleRemoveFromCart={handleRemoveFromCart}
+        user={user}
+        products={cartProducts}
+      />
+      <CartSummary products={cartProducts} />
     </Segment>
   );
 }

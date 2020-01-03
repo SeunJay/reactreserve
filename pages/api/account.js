ec0d7125c93c17175f99c7b6f2316e383de8aba3
seun jay <seunjay92@gmail.com>
2020-01-04 00:13:13 +0100

    user role change

--- pages/api/account.js
+++ pages/api/account.js
@@ -5,6 +5,19 @@ import connectDB from "../../utils/connectDb";
 connectDB();
 
 export default async (req, res) => {
+  switch (req.method) {
+    case "GET":
+      await handleGetRequest(req, res);
+      break;
+    case "PUT":
+      await handlePutRequest(req, res);
+      break;
+    default:
+      res.status(405).send(`Method ${req.method} not allowed`);
+  }
+};
+
+async function handleGetRequest(req, res) {
   if (!("authorization" in req.headers)) {
     return res.status(401).send("No authorization token!");
   }
@@ -21,6 +34,12 @@ export default async (req, res) => {
 
     res.status(200).json(user);
   } catch (error) {
-    res.status(403).send("Invalid Token!")
+    res.status(403).send("Invalid Token!");
   }
-};
+}
+
+async function handlePutRequest(req, res) {
+  const { _id, role } = req.body;
+  await User.findOneAndUpdate({ _id }, { role });
+  res.status(200).send("User role updated");
+}
